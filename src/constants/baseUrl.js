63013e3c0174317a397cfbebd02f66d27bdf741f
KingAbesh <abeshekwere@gmail.com>
2020-10-15 21:18:08 +0100

    chore: set up redirects for netlify & update base url for prod

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,1 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8080/api/v1" : ""
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:8080/api/v1"
+    : "https://techinnover-server.herokuapp.com/api/v1";
